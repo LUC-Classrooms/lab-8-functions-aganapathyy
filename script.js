b38e77db22a899ca40bfeeed8a7e179d01e72dcc
Anya Ganapathy <156939078+aganapathyy@users.noreply.github.com>
2024-03-04 18:51:27 +0000

    lab 8 submitted

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ function draw() {
   background(200);
 
   //this is a call to myShape()
-  myShape(width / 2, height / 2, 1);
+  myShape(width/2, height/ 2, 1);
   // myShape() takes three arguments
+  myShape(100,200,60,80)
+  myShape(400,100,70,100)
+  myShape(150,200,50,70)
+  myShape(300,200,40,60)
   // arg1: horizontal location
   // arg2: vertical location
   // arg3: scale factor (1 is full size)
@@ -16,11 +20,11 @@ function draw() {
   
 }
 
-function myShape(x, y, s) {
+function myShape(x, y, w, h) {
   // make this function more interesting
   push(); // make a separate layer
   translate(x, y); // move the origin point
-  scale(s);
+  scale(.5);
   
   ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
   
